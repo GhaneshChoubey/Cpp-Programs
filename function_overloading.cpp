d915953eecae0399c4da3b10c7978c7cf858286a
GhaneshChoubey <anuchomah@gmail.com>
2024-03-02 23:59:54 +0530

    first

---
+++ function_overloading.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int area(int,int);
+float area(int);
+int main()
+{
+    int r;
+    cout<<"Enter radius of a circle";
+    cin>>r;
+    float A=area(r);
+    cout<<"\n Area of circle is "<<A;
+    int l,b,a;
+    cout<<"\n Enter length and breadth of rectangle";
+    cin>>l>>b;
+    area(l,b);
+    Cout
+}
