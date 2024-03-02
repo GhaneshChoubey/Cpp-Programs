739d579e16e9c39b272ded7c787829a698794d15
GhaneshChoubey <anuchomah@gmail.com>
2024-03-03 00:04:08 +0530

    first

--- function_overloading.cpp
+++ function_overloading.cpp
@@ -7,13 +7,23 @@ float area(int);
 int main()
 {
     int r;
-    cout<<"Enter radius of a circle";
+    cout<<"Enter radius of a circle:-";
     cin>>r;
     float A=area(r);
-    cout<<"\n Area of circle is "<<A;
+    cout<<"\n Area of circle is :-"<<A;
     int l,b,a;
-    cout<<"\n Enter length and breadth of rectangle";
+    cout<<"\n Enter length and breadth of rectangle:-";
     cin>>l>>b;
-    area(l,b);
-    Cout
+    a=area(l,b);
+    cout<<"Area of rectangle is:-"<<a;
+    return 0;
+}
+
+float area(int R)
+{
+    return(3.14*R*R);
+}
+int area(int L,int B)
+{
+    return (L*B);
 }
